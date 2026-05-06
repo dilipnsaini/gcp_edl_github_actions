92114b121e5bdf32e7f5a386e0b37f91a0c07fb4
Dilip Saini <167434150+dilipnsaini@users.noreply.github.com>
2026-05-06 21:55:25 +0530

    Create update_edl.py

---
+++ update_edl.py
@@ -0,0 +1,11 @@
+import requests
+
+URL = "https://www.gstatic.com/ipranges/cloud.json"
+OUTPUT = "gcp_edl.txt"
+
+data = requests. Get(URL, timeout=10).json()
+
+with open(OUTPUT, "w") as f:
+   for entry in data["prefixes"]:
+       if "ipv4Prefix" in entry:
+           f.write(entry["ipv4Prefix"] + "\n")
