ffd94735341cbb5ca8247065dc3cbfb2ad201e5e
Dilip Saini <167434150+dilipnsaini@users.noreply.github.com>
2026-05-06 22:03:08 +0530

    Update update_edl.py

--- update_edl.py
+++ update_edl.py
@@ -3,7 +3,7 @@ import requests
 URL = "https://www.gstatic.com/ipranges/cloud.json"
 OUTPUT = "gcp_edl.txt"
 
-data = requests. Get(URL, timeout=10).json()
+data = requests.get(URL, timeout=10).json()
 
 with open(OUTPUT, "w") as f:
    for entry in data["prefixes"]:
